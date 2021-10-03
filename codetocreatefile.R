5566b40fd44d0b3546864e6b776f0b7ca388bfda
Craig Lazarski <Craig_Lazarski@caryacademy.org>
2021-10-03 12:30:05 -0400

    I am so done with this!

--- codetocreatefile.R
+++ codetocreatefile.R
@@ -1,3 +1,3 @@
 # Script for rendering the README fild
 
-rmarkdown::render(input = 'README.Rmd', output_format = 'github_document', output_options = list(  keep_html = FALSE))
+rmarkdown::render(input = 'README.Rmd', output_format = 'github_document', output_options = list(html_preview = FALSE))
